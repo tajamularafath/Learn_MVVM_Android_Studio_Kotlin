f6572359e20c086db3a26d2ce82c45094f684343
Tajamul Arafath <vptajju@gmail.com>
2024-05-10 17:19:57 +0530

    Added Viewmodel

--- app/src/main/java/com/example/mvvmproject/MainActivity.kt
+++ app/src/main/java/com/example/mvvmproject/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.mvvmproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.mvvmproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+    lateinit var myViewmodel : MyViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        myViewmodel = ViewModelProvider(this).get(MyViewModel::class.java)
+
+        binding.textViewCounter.text = myViewmodel.getInitialCounter().toString()
+
+        binding.buttonIncrement.setOnClickListener {
+            binding.textViewCounter.text = myViewmodel.getUpdatedCounter().toString()
+        }
     }
 }
